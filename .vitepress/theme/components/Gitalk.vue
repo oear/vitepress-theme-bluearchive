e1c3457ab289b7f085edd2e065672c2a2017fa21
oear <sourcepond@outlook.com>
2025-02-13 22:23:03 +0800

    hide ClientID

--- .vitepress/theme/components/Gitalk.vue
+++ .vitepress/theme/components/Gitalk.vue
@@ -8,35 +8,53 @@
 import { onMounted } from 'vue'
 import { useData } from 'vitepress'
 import md5 from 'md5'
+
+// 获取 VitePress 配置
 const themeConfig = useData().theme.value
 declare var Gitalk: any
-onMounted(() => {
-  const commentConfig = {
-    clientID: themeConfig.clientID,
-    clientSecret: themeConfig.clientSecret,
-    repo: themeConfig.repo,
-    owner: themeConfig.owner,
-    admin: themeConfig.admin,
-    id: md5(location.pathname).toString(),
-    distractionFreeMode: false,
-  }
 
-  const gitalk = new Gitalk(commentConfig)
-  gitalk.render('gitalk-container')
+onMounted(() => {
+  // 检查 Gitalk 是否已经渲染，防止重复渲染
+  if (document.getElementById('gitalk-container').children.length === 0) {
+    // 配置 Gitalk 参数
+    const commentConfig = {
+      clientID: import.meta.env.CLIENT_ID, // 从环境变量中获取 Client ID
+      clientSecret: import.meta.env.CLIENT_SECRET, // 从环境变量中获取 Client Secret
+      repo: themeConfig.repo,  // 使用从配置中获取的仓库名称
+      owner: themeConfig.owner,  // 使用从配置中获取的仓库拥有者
+      admin: themeConfig.admin,  // 使用从配置中获取的管理员列表
+      id: md5(location.pathname).toString(), // 使用 MD5 对页面路径进行哈希生成唯一 ID
+      distractionFreeMode: false, // 启用干扰自由模式
+    }
+
+    try {
+      const gitalk = new Gitalk(commentConfig)
+      gitalk.render('gitalk-container')  // 渲染评论区到指定容器
+    } catch (error) {
+      console.error("Gitalk 渲染失败: ", error)
+    }
+  }
 })
 </script>
 
-<style>
-.gt-container .gt-header-textarea {
-  color: var(--font-color-grey);
-  background-color: var(--general-background-color) !important;
-  transition: background-color 0.5s, color 0.5s !important;
-  
+<style scoped>
+.gitalk-container {
+  margin-top: 20px;
+  padding: 20px;
+  border-top: 2px solid #f1f1f1;
 }
-.gt-container .gt-comment-content{
-  background-color: var(--foreground-color) !important;
-  p{
-    color: var(--font-color-grey);
-  }
+
+.gt-container .gt-header-textarea {
+  color: var(--font-color-grey);  /* 自定义字体颜色 */
+  background-color: var(--general-background-color) !important; /* 自定义背景色 */
+  transition: background-color 0.5s, color 0.5s !important; /* 平滑过渡 */
+}
+
+.gt-container .gt-comment-content {
+  background-color: var(--foreground-color) !important; /* 自定义评论内容区域背景色 */
+}
+
+.gt-container .gt-comment-content p {
+  color: var(--font-color-grey); /* 自定义评论内容字体颜色 */
 }
 </style>
